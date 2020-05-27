d5c855531af153e26577256ba4831990a73a5890
kalsnaie <kalsnaie@yahoo.com>
2020-05-27 03:57:44 +0300

    strings commit

--- strings.py
+++ strings.py
@@ -1,4 +1,7 @@
 first_name = 'KHALID'
-last_name = 'alsnaiE'
+last_name = 'Abdullatif ali'
 print(first_name.lower() + " " + last_name.upper())
+print(last_name.replace('ali','ibrahim'))
+b = 'A' * 500
+print(b)
 
