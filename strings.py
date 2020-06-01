511b99b42ca8108682fac94f2e37b1bfdabc18e0
kalsnaie <kalsnaie@yahoo.com>
2020-06-02 00:38:07 +0300

    strings

--- strings.py
+++ strings.py
@@ -1,6 +1,6 @@
-first_name = 'KHALID'
+first_name = 'KHALID\n'
 last_name = 'Abdullatif ali'
-print(first_name.lower() + " " + last_name.upper())
+print(first_name.lower()  + last_name.upper())
 print(last_name.replace('ali','ibrahim'))
 b = 'A' * 500
 print(b)
